479ee7d5999d72f6297ae47107d03da6c1a207e8
viktoriakotejko <viktoria.kotejko@gmail.com>
2017-09-18 21:10:55 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'acsm_94b3dbc760e5943');
+define('DB_NAME', 'ef');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'b009391dfb8e22');
+define('DB_USER', 'root');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'YES');
+define('DB_PASSWORD', 'Vika23132009');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localdb');
